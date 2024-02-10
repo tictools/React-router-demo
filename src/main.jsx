c30120e74d0f1704f552e53bdf0163ef087b446d
tictools <jaumevillarreal@gmail.com>
2024-02-10 18:19:49 +0100

    feat: add paths alias + update imports + create data and api folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import ErrorPage from "./ErrorPage";
+
+import { ErrorPage } from "@components";
+import { App, Contact } from "@routes";
+
 import "./index.css";
-import { App, Contact } from "./routes";
 
 const router = createBrowserRouter([
   {
