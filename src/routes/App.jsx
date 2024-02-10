cf2f06561fb1e8ff2a165358ba12d80763ec6f6b
tictools <jaumevillarreal@gmail.com>
2024-02-10 19:42:49 +0100

    feat: extract components

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+import { ContactForm, ContactList, EmptyContactList } from "@src/components";
 import { useContactsDataLoader } from "@src/hooks";
-import { Link, Outlet } from "react-router-dom";
 
 export const App = function () {
   const { contacts, hasContacts } = useContactsDataLoader();
@@ -8,36 +10,12 @@ export const App = function () {
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
+        <ContactForm />
         <nav>
           {hasContacts ? (
-            <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
+            <ContactList contacts={contacts} />
           ) : (
-            <p>
-              <i>No contacts</i>
-            </p>
+            <EmptyContactList />
           )}
         </nav>
       </div>
