ce807ebfa842c96f14a179a4833e08ab08e4f016
tictools <jaumevillarreal@gmail.com>
2024-03-14 19:32:08 +0100

    feat: implement submit on change + useUpdateRefValueBy hook

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,15 +1,28 @@
-import { Form } from "react-router-dom";
+import { useContactsLoaderData, useUpdateRefValueBy } from "@src/hooks";
+import { Form, useSubmit } from "react-router-dom";
 
 export const ContactForm = function () {
+  const { query } = useContactsLoaderData();
+  const submit = useSubmit();
+
+  const { inputRef } = useUpdateRefValueBy({ query });
+
+  const handleSubmit = (event) => {
+    submit(event.currentTarget.form);
+  };
+
   return (
     <div>
       <Form id="search-form" role="search">
         <input
+          ref={inputRef}
           id="q"
           aria-label="Search contacts"
           placeholder="Search"
           type="search"
           name="q"
+          defaultValue={query}
+          onChange={handleSubmit}
         />
         <div id="search-spinner" aria-hidden hidden={true} />
         <div className="sr-only" aria-live="polite"></div>
