bfda40de1f9064cc2be9e37956ac93973d85f367
tictools <jaumevillarreal@gmail.com>
2024-03-15 12:43:17 +0100

    feat: customize error response if contact not found

--- src/loaders/contactLoader.js
+++ src/loaders/contactLoader.js
@@ -1,6 +1,14 @@
 import { getContact } from "@src/api/contacts";
+import { throwResponse } from "@src/utils";
 
 export const contactLoader = async function ({ params }) {
   const contact = await getContact(params.contactId);
+
+  if (!contact)
+    throwResponse({
+      status: 404,
+      statusText: "Contact not found",
+    });
+
   return { contact };
 };
